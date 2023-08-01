358e52eacbb6975fd7fdd23cebc569ed265f1369
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-08-01 16:21:10 +0200

    Add extended enumeration definitions to CuraEngine infill.
    In response to CURA-10916, additional enumeration definitions were added, specifically for CuraEngine's infill patterns.
    This update will ensure that every time a container is loaded, if it's a 'definition container' and not an 'extruder', all infill patterns present will extend the category with "UM Logo".
    This will effectively link these infill patterns to the 'curaengine_plugin_infill_generate' plugin.
    Be mindful there is also a pending task to only start the plugin if the extended enums are actually being used.
    
    CURA-10916

--- CuraEngineInfillGenerate/CuraEngineInfillGenerate.py
+++ CuraEngineInfillGenerate/CuraEngineInfillGenerate.py
@@ -6,6 +6,8 @@ from pathlib import Path
 
 from UM.i18n import i18nCatalog
 from UM.Logger import Logger
+from UM.Settings.ContainerRegistry import ContainerRegistry
+from UM.Settings.DefinitionContainer import DefinitionContainer
 from cura.BackendPlugin import BackendPlugin
 
 catalog = i18nCatalog("cura")
@@ -23,6 +25,26 @@ class CuraEngineInfillGenerate(BackendPlugin):
             self._plugin_command = [self.binaryPath().as_posix()]
 
         self._supported_slots = [200]  # ModifyPostprocess SlotID
+        ContainerRegistry.getInstance().containerLoadComplete.connect(self._on_container_load_complete)
+
+    def _on_container_load_complete(self, container_id) -> None:
+        if not ContainerRegistry.getInstance().isLoaded(container_id):
+            # skip containers that could not be loaded, or subsequent findContainers() will cause an infinite loop
+            return
+        try:
+            container = ContainerRegistry.getInstance().findContainers(id = container_id)[0]
+        except IndexError:
+            # the container no longer exists
+            return
+        if not isinstance(container, DefinitionContainer):
+            # skip containers that are not definitions
+            return
+        if container.getMetaDataEntry("type") == "extruder":
+            # skip extruder definitions
+            return
+        for infill_patterns in ("infill_pattern", "support_pattern", "support_interface_pattern", "support_roof_pattern", "support_bottom_pattern", "roofing_pattern", "top_bottom_pattern", "ironing_pattern"):
+            for definition in container.findDefinitions(key = infill_patterns):
+                definition.extend_category("logo", "UM Logo", plugin_id="curaengine_plugin_infill_generate", plugin_version="0.1.0")
 
     def getPort(self):
         return super().getPort() if not self.isDebug() else int(os.environ["CURAENGINE_INFILL_GENERATE_PORT"])
@@ -31,6 +53,7 @@ class CuraEngineInfillGenerate(BackendPlugin):
         return not hasattr(sys, "frozen") and os.environ.get("CURAENGINE_INFILL_GENERATE_PORT", None) is not None
 
     def start(self):
+        # TODO: Only start the plugin if any of the extended enums are actually used.
         if not self.isDebug():
             super().start()
 
